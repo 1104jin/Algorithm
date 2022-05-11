a4ed2224c007fc3839a5c1fd0f8c56845a14a3fe
NOO00OON <1104_jin@naver.com>
2022-05-11 22:01:31 +0900

    Create Main_9205.java

---
+++ BOJ/Main_9205.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main{
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+
+		for (int t = 0; t < T; t++) {
+			int n = sc.nextInt();
+
+			int[] x = new int[n + 2];
+			int[] y = new int[n + 2];
+			int[][] map = new int[n + 2][n + 2];
+
+			for (int i = 0; i < n + 2; i++) {
+				x[i] = sc.nextInt();
+				y[i] = sc.nextInt();
+			}
+
+			for (int i = 0; i < n + 2; i++) {
+				map[i][i] = 1; 
+				for (int j = i + 1; j < n + 2; j++) {
+					int d = Math.abs(x[i] - x[j]) + Math.abs(y[i] - y[j]);
+					if (d <= 1000)
+						map[i][j] = map[j][i] = 1;
+				}
+			}
+
+			for (int k = 0; k < n + 2; k++) {
+				for (int i = 0; i < n + 2; i++) {
+					for (int j = 0; j < n + 2; j++) {
+						if (map[i][k] == 1 && map[k][j] == 1)
+							map[i][j] = 1;
+					}
+				}
+			}
+			if(map[0][n+1] == 1)
+				System.out.println("happy");
+			else
+				System.out.println("sad");
+				
+		}
+	}
+}
