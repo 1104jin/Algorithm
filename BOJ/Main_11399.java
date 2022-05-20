21a88d6c6a74bd6e8c6ae7184df2760472a87095
NOO00OON <1104_jin@naver.com>
2022-05-20 20:34:46 +0900

    Create Main_11399.java

---
+++ BOJ/Main_11399.java
@@ -0,0 +1,25 @@
+ java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int[] map = new int[N+1];
+
+		for (int i = 1; i < N+1; i++) {
+			map[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(map);
+		int total = 0;
+		for (int i = 1; i < map.length; i++) {
+			total += (map[i]*(N-i+1));
+		}
+		
+		System.out.println(total);
+	}
+
+}
